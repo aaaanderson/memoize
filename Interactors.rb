17c120342df3e915088b4f13f726b627aec2b5aa
Andy Anderson <ae.anderson0@gmail.com>
2017-04-12 18:56:12 -0500

    well i cache stuff now

--- Interactors.rb
+++ Interactors.rb
@@ -3,20 +3,26 @@ require 'digest/sha1'
 module Memoize
 	attr :lookup
 
-	def initialize
-		puts "Initializing lookup table"
-		lookup = {}
-		
-	end
+#	def initialize
+#		puts "Initializing lookup table"
+#		@lookup = {}
+#	end
 
 	def call(*args)
+		puts "Entering lookup"
+
+		# had a hard time getting initialize to work
+		if @lookup.nil?
+			@lookup = {}
+		end
+
 		stringified_args = args.join(" ")
 
-		val = lookup[stringified_args]
+		val = @lookup[stringified_args]
 		if val.nil?
 			puts "Caching " + stringified_args
 			calculated_val = super args
-			lookup[stringified_args] = calculated_val
+			@lookup[stringified_args] = calculated_val
 			return calculated_val
 		end
 		return val
@@ -25,8 +31,8 @@ end
 
 module Interactors
 
+
 	class ImportantThing
-		include Memoize
 
 		def initialize(*args)
 			puts "Creating important thing"
@@ -41,4 +47,8 @@ end
 
 i = Interactors::ImportantThing.new
 
+i.extend(Memoize)
+
 puts i.call(["foo", "bar", "bat"])
+puts i.call(["foo", "bar", "bat"])
+puts i.call(["foo", "bar", "baz"])
